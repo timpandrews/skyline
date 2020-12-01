7dc407bedfd34f0f26bdaf663e8eaef2456c4919
Tim Andrews <timpandrews@yahoo.com>
2020-12-01 11:38:34 -0500

    signup

--- skyline/settings.py
+++ skyline/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third-party apps
     'crispy_forms',
-    'rides'
+    # apps
+    'accounts',
+    'rides',
+
 ]
 
 MIDDLEWARE = [
